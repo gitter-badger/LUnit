6b3d00b7a378b33f6ea8b32a6a129c5346c7da83
Code Singularity <codesingularity@outlook.com>
2016-08-24 19:34:43 -1000

    update

--- Tests LUnit/LUnitMarkdownGenerator.cs
+++ Tests LUnit/LUnitMarkdownGenerator.cs
@@ -15,13 +15,13 @@ namespace LCore.LUnit.Tests
         /// <summary>
         /// Override this member to specify the assemblies to generae documentation.
         /// </summary>
-        protected override Assembly[] DocumentAssemblies => new[] { Assembly.GetAssembly(typeof(LUnit)) };
+        public override Assembly[] DocumentAssemblies => new[] { Assembly.GetAssembly(typeof(LUnit)) };
 
-        protected override void WriteIntro(GitHubMarkdown MD)
+        public override void Home_Intro(GitHubMarkdown MD)
             {
             }
 
-        protected override void HowToInstall(GitHubMarkdown MD)
+        public override void HowToInstall(GitHubMarkdown MD)
             {
             MD.Line($"Add {nameof(LCore.LUnit)} as a nuget package:");
             MD.Code(new[] { $"Install-Package {nameof(LCore.LUnit)}" });
@@ -30,25 +30,25 @@ namespace LCore.LUnit.Tests
         /// <summary>
         /// Override this value to display a large image on top ofthe main document
         /// </summary>
-        protected override string BannerImage_Large(GitHubMarkdown MD) =>
+        public override string BannerImage_Large(GitHubMarkdown MD) =>
             MD.GetRelativePath($"{typeof(LUnit).GetAssembly().GetRootPath()}\\Content\\{nameof(LCore.LUnit)}-banner-large.png");
 
         /// <summary>
         /// Override this value to display a small banner image on top of sub-documents
         /// </summary>
-        protected override string BannerImage_Small(GitHubMarkdown MD) =>
+        public override string BannerImage_Small(GitHubMarkdown MD) =>
             MD.GetRelativePath($"{typeof(LUnit).GetAssembly().GetRootPath()}\\Content\\{nameof(LCore.LUnit)}-banner-small.png");
 
         /// <summary>
         /// Override this value to display a large image in the upper right corner of the main document
         /// </summary>
-        protected override string LogoImage_Large(GitHubMarkdown MD) =>
+        public override string LogoImage_Large(GitHubMarkdown MD) =>
             MD.GetRelativePath($"{typeof(LUnit).GetAssembly().GetRootPath()}\\Content\\{nameof(LCore.LUnit)}-logo-small.png");
 
         /// <summary>
         /// Override this value to display a small image in the upper right corner of sub-documents
         /// </summary>
-        protected override string LogoImage_Small(GitHubMarkdown MD) =>
+        public override string LogoImage_Small(GitHubMarkdown MD) =>
             MD.GetRelativePath($"{typeof(LUnit).GetAssembly().GetRootPath()}\\Content\\{nameof(LCore.LUnit)}-logo-small.png");
         }
     }
